b4107c87cebd89f7fa2e1dd3fcd9948631fccc17
theone056 <32253665+theone056@users.noreply.github.com>
2022-03-16 22:12:18 +0800

    task tracker project

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  Tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTask().subscribe((task) => (this.Tasks = task));
+  }
+}
